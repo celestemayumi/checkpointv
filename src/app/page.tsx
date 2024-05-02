da1f56e96b555992d20965f5827951e8bfe8f654
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-02 18:38:46 -0300

    ♻️ refactor: header component in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import PopularProducts from "@/components/PopularProducts";
 import Footer from "@/components/Footer";
@@ -6,7 +5,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main>
-      <Header/>
       <HeroSection/>
       <PopularProducts/>
       <Footer/>
