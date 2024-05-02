3f26bb419072775fc07977acc5e59822fbd16e8a
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-02 20:15:58 -0300

    💄 style: comportamento do scroll na pagina produtos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth scroll-pt-20">
       <body className={inter.className}>
       <Header/>
         {children}</body>
