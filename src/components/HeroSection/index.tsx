ebc69fe36a1c133d3ec0bfc5445c35b16d31fb0f
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-02 10:47:38 -0300

    style: componente herosection atualizado

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 
 const HeroSection = ()=> {
     return(
-        <section>
+        <section className="bg-[#eaeeef] shadow-md flex justify-evenly p-6 items-center">
+            <div className="w-1/3">
+            <h1 className="text-8xl font-bold montserrat    ">SALE!</h1>
+            <p className="montserrat text-xl">Descontos com os <span className="font-bold">best-sellers</span> e itens <span className="font-bold">exclusivos</span> aqui na HEAT.</p>
+            </div>
             <Image
-            src='/main.png'
-            width={1400}
-            height={1000}
+            className=""
+            src='/sneakers/campus2.png'
+            width={450}
+            height={10}
             alt="hero section"
-            className="shadow-md"
             ></Image>
         </section>
     )
