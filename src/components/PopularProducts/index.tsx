72f25aee79664ca6dcf24c809bc60b8e3d4b137e
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-02 11:32:43 -0300

    style: componente produtos na homepage

--- src/components/PopularProducts/index.tsx
+++ src/components/PopularProducts/index.tsx
@@ -6,20 +6,23 @@ import Image from "next/image";
 const PopularProducts = () => {
   return (
     <section className="montserrat">
+      <div className="flex items-center ">
       <h1 className="font-semibold m-5 text-2xl">Produtos Populares</h1>
-      <div className="flex flex-wrap">
-        <div className="flex flex-wrap gap-3 justify-center">
-        {sneakers.slice(0, 5).map((sneaker, id) => {
+      <button className="montserrat underline underline-offset-2"><a href="/produtos">Veja mais produtos</a></button>
+      </div>
+      <div className="flex flex-wrap justify-evenly">
+        {sneakers.slice(0, 6).map((sneaker, id) => {
             return (
-              <div className="w-48" key={id}>
-                <Image src={sneaker.image} width={200} height={50} alt="Icone da tecnologia" />
-                <p className="font-bold tracking-wide ">{sneaker.name}</p>
-                <p>{sneaker.price}</p>
+              <div className="w-52 flex flex-col p-5 border rounded-md justify-evenly space-y-6" key={id}>
+                <Image src={sneaker.image} width={300} height={50} alt="Icone do tênis" className="" />
+                <div className="">
+                <p className="font-semibold tracking-wide">{sneaker.name}</p>
+                <p className="font-bold ">R${sneaker.price}</p>
+                </div>
               </div>
             );
           })}
         </div>
-      </div>
     </section>
   );
 };
