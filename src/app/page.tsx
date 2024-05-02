0b72e5d92470dc803c3e5d39f5116223a5440b58
Celeste Tanaka <cehmayumi@gmail.com>
2024-05-02 11:46:03 -0300

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import PopularProducts from "@/components/PopularProducts";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header/>
       <HeroSection/>
       <PopularProducts/>
+      <Footer/>
     </main>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+const Footer = ()=>{
+    return(
+        <footer className=" bg-slate-200 flex montserrat justify-evenly p-4 text-sm">
+             <p>&copy; 2024 Celeste Tanaka. Todos os direitos reservados.</p>
+             <a href="https://www.linkedin.com/in/celestetanaka/" className="font-semibold">Linkedin</a>
+        </footer>
+    )
+}
+
+export default Footer;
